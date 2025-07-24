277568ce6862e0c747290d30c1e468c17a7d29f2
Bhanu3486 <149360361+Bhanu3486@users.noreply.github.com>
2025-07-25 01:07:59 +0530

    Add files via upload

---
+++ EvenCheckerTest.java
@@ -0,0 +1,24 @@
+package junit;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EvenCheckerTest {
+	
+	    EvenChecker checker = new EvenChecker();
+
+	    @ParameterizedTest
+	    @ValueSource(ints = {2, 4, 6, 8, 10})
+	    public void testEvenNumbers(int input) {
+	        assertTrue(checker.isEven(input));
+	    }
+
+	    @ParameterizedTest
+	    @ValueSource(ints = {1, 3, 5, 7, 9})
+	    public void testOddNumbers(int input) {
+	        assertFalse(checker.isEven(input));
+	    }
+	}
+
+
